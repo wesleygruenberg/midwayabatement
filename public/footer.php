777e5aff80a3f1a572ea0cb4635aa63a36f3f97d
Wesley Gruenberg <wesleygruenberg@u.boisestate.edu>
2018-03-21 15:51:55 -0600

    mobile scalability

--- public/footer.php
+++ public/footer.php
@@ -2,7 +2,7 @@
 		<footer class = "page-footer">
 		<div class = "footer-container">
 		<ul class = "footer-list">
-			<li>&copy; 2018 WG3 Web Design</li>
+			<li><a href="http://www.clarke.com/">&copy; 2018 WG3 Web Design</a></li>
 			<li><a href="mailto:wgruenberg@clarke.com">Wes Gruenberg - Site Manager</a> </li>
 			<li class = "last-child"><a href="http://www.clarke.com">The Clarke Group, Inc.</a></li>
 		</ul>
