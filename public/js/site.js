e06d9fba2f2dfe72e4ea8ec5d0e117bda8015ad3
Wesley Gruenberg <wesleygruenberg@u.boisestate.edu>
2018-06-09 17:39:43 -0600

    fixing maps

--- public/js/site.js
+++ public/js/site.js
@@ -5,26 +5,26 @@
 window.onload = function(e) {
 
 	
-var slideIndex = 0;
-showSlides();
+	var slideIndex = 0;
+	showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-			
+	function showSlides() {
+		var i;
+		var slides = document.getElementsByClassName("mySlides");
+		var dots = document.getElementsByClassName("dot");
+		for (i = 0; i < slides.length; i++) {
+		   slides[i].style.display = "none";  
+		}
+		slideIndex++;
+		if (slideIndex > slides.length) {slideIndex = 1}    
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slides[slideIndex-1].style.display = "block";  
+		dots[slideIndex-1].className += " active";
+		setTimeout(showSlides, 2000); // Change image every 2 seconds
+	}
+				
 	
 }
 
@@ -39,7 +39,7 @@ function initMap() {
 		  mapTypeId: google.maps.MapTypeId.HYBRID
         });
 		var madLayer = new google.maps.KmlLayer({
-          url: 'http://cs401-website-wesleygruenberg.herokuapp.com/MAD_BOUNDARIES.KML',
+          url: 'https://midwayabatement.herokuapp.com/MAD_BOUNDARIES.KML',
           map: map
         });
 		  var marker = new google.maps.Marker({
@@ -47,7 +47,7 @@ function initMap() {
           map: map
         });
 		var countyBoundaryLayer = new google.maps.KmlLayer({
-          url: 'http://cs401-website-wesleygruenberg.herokuapp.com/JEFFERSON_COUNTY_BOUNDARY.KML',
+          url: 'https://midwayabatement.herokuapp.com/JEFFERSON_COUNTY_BOUNDARY.KML',
           map: map
         });
 
@@ -136,73 +136,5 @@ function initMap() {
 		
 }
 
-function initAutocomplete() {
-
-
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-
-          if (places.length == 0) {
-            return;
-          }
-
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
-
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-      }
-
-function initialize() {
-
-   initMap();
-
-}
 
 
